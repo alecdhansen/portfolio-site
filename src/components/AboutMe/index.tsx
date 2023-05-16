81612322dfc1a14556511c1820506a044832af88
Alec Hansen <alecdhansen@gmail.com>
2023-05-16 12:55:17 -0400

    add current work status in bio

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -9,6 +9,7 @@ import { Props } from "../../utils/types";
 import Form from "../Form";
 import { motion } from "framer-motion";
 import { content, mobileContent } from "../Portfolio/utils";
+import { CompanyTag } from "../Portfolio/style";
 
 const AboutMe: React.FC<Props> = ({ c1, c2 }) => {
   const mobile = window.innerWidth < 480;
@@ -32,12 +33,14 @@ const AboutMe: React.FC<Props> = ({ c1, c2 }) => {
           residing in Greenville, South Carolina, USA. Coding has changed my
           life! My previous career gave me a strong background in client care
           and team management. I now use these skills to build web and mobile
-          applications with a great attention to detail and (when with a team) I
-          collaborate with others thoughtfully and efficiently.
+          applications with a great attention to detail and I (when with a team)
+          collaborate thoughtfully and efficiently.
           <br />
           <br />
-          I'm looking for my next opportunity. Let me generate solutions that
-          fit your needs!
+          I'm currently working full-time on a React Native application for{" "}
+          <CompanyTag href="https://purplewrendigital.com/" target="_blank">
+            Purple Wren Digital.
+          </CompanyTag>{" "}
         </BlurbContainer>
         <Underline>
           <Link
